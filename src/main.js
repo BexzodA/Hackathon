7ae231bc497dfae690fb94cde75319bde1212d76
Bexzod Abduvaliev <babduvaliev@kythera.space>
2023-02-25 16:47:57 -0500

    set up project structure.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
-// Hi Henry
 "use strict"
 const app = new PIXI.Application({
     width: 800,
-    height: 600
+    height: 600 
 });
 onload = ()=> {
     document.body.append(app.view);
+    setup();
+}
+
+let stage;
+let mainMenu;
+let scene1;
+
+function transitionToScene(from, to) {
+    app.stage.removeChild(from.getScene());
+    app.stage.addChild(to.getScene());
 }
 
 function setup() {
+    stage = app.stage;
+    var graphics = new PIXI.Graphics();
+    graphics.lineStyle(5, 0xFF0000);
+    graphics.drawRect(3, 2, 795, 595);
+    stage.addChild(graphics);
 
+    let mainMenu = new MainMenu(stage);
 }
---
+++ src/scenes/MainMenu.js
@@ -0,0 +1,33 @@
+
+class MainMenu {
+    constructor(_stage) {
+        this.mainMenu = new PIXI.Container();
+        this.stage = _stage;
+
+        let title = PIXI.Sprite.from("../imgs/title.png");
+        title.x = 4;
+        title.y = 3;
+        title.width = 792;
+        title.height = 593;
+        this.mainMenu.addChild(title);
+
+        let playButtonLabel = PIXI.Sprite.from("../imgs/playButton.png");
+        playButtonLabel.x = 300;
+        playButtonLabel.y = 450;
+        playButtonLabel.buttonMode = true;
+        playButtonLabel.interactive = true;
+        playButtonLabel.on("pointerup", this.nextScene.bind(this)); //startGame is a function reference
+        playButtonLabel.on("pointerover", e => e.target.alpha = 0.7); //concise arrow func with now brackets
+        playButtonLabel.on("pointerout", e => e.currentTarget.alpha = 1.0);
+        this.mainMenu.addChild(playButtonLabel);
+        this.stage.addChild(this.mainMenu);
+    }
+
+    getScene() {
+        return this.mainMenu;
+    }
+
+    nextScene() {
+        transitionToScene(this, new Scene1(this.stage));
+    }
+}
